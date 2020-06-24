4fe1eadc328b5d5eac790e47f2b97b419a40c11a
cmndcntrlcyber <47669879+cmndcntrlcyber@users.noreply.github.com>
2020-06-23 20:25:52 -0500

    Create fave.py

---
+++ fave.py
@@ -0,0 +1,6 @@
+import os
+import process
+
+process = subprocess.Popen([os.system('bash', '/home/d43d3lu5/shell-scripts/vuln.sh')])
+os.system('bash', '/home/d43dl3u5/shell-scripts/web-vuln.sh'))
+os.system('bash', '/home/d43dl3u5/shell-scripts/dir.sh'))
